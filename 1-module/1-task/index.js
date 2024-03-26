0e68c03064bc067080b71b683602639aac10fc98
DzmitryRamashka <DzmitryRamashka@github.com>
2024-03-26 16:37:49 +0300

    добавлена функция расчёта факториала

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,16 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1;
+  if (n >= 0) {
+    for(let i = n; i > 1; i--) {
+      result = result*i;
+    }
+    }
+  else {
+    result = "число не должно быть отрицательным";
+  }
+    
+  return result
 }
+
+
+
