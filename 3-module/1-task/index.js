da620815502b2fe35d8a86c83dc2171c5fa1ff32
DzmitryRamashka <DzmitryRamashka@github.com>
2024-03-31 21:25:24 +0300

    Рефакторинг: использование map

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,7 +1,3 @@
 function namify(users) {
-  let names = [];
-  for (let i = 0; i < users.length; i++) {
-    names[i] = users[i].name;
-  }
-  return names;
+  return users.map(item => item.name);
 }
