0a92274646b370dbe03128863fbb71146ef01713
DzmitryRamashka <DzmitryRamashka@github.com>
2024-04-02 15:07:58 +0300

    Вывод многострочной строки

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,23 @@
 function showSalary(users, age) {
-  // ваш код...
+  let counter = 0;
+  let count = 0;
+  let str = '';
+  for (const user of users) {
+    if (user.age <= age) {
+      counter ++;
+    }
+  }
+  for (const user of users) {
+    if (user.age <= age) {
+      count ++;
+      if (count != counter) {
+        str += user.name + ', ' + user.balance + '\n';
+
+      } else {
+        str += user.name + ', ' + user.balance;
+      }
+    }
+
+  }
+  return str
 }
