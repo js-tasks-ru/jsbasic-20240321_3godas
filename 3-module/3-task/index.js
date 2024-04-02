c6a498c772aa48a8761014391a2f79d3b175e0cc
DzmitryRamashka <DzmitryRamashka@github.com>
2024-04-02 11:45:01 +0300

    Убрать тире в сделать заглавные буквы

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,12 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('');
+  
+  for (index of arr.keys()) {
+    if (arr[index] == '-') {
+      arr[index+1] = arr[index+1].toUpperCase();
+    }
+  }
+
+  return arr.filter(char => char != '-').join('')
+
 }
