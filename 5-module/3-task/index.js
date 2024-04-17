376ed644e7b21e09f67df3e5e016f817b9627215
DzmitryRamashka <DzmitryRamashka@github.com>
2024-04-17 17:19:47 +0300

    Added carousel

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,44 @@
 function initCarousel() {
-  // ваш код...
+  const carouselInner = document.querySelector('.carousel__inner');
+  const arrowLeft = document.querySelector('.carousel__arrow_left');
+  const arrowRight = document.querySelector('.carousel__arrow_right');
+  const offcetWidth = carouselInner.offsetWidth;
+
+  let currentFrame = 0;
+
+  function showButtons() {
+    if (currentFrame === 0) {
+      arrowLeft.style.display = 'none';
+      arrowRight.style.display = '';
+
+    } else if (currentFrame === 3) {
+      arrowLeft.style.display = '';
+      arrowRight.style.display = 'none';
+    } else {
+      arrowLeft.style.display = '';
+      arrowRight.style.display = '';
+    }
+
+
+  }
+
+  showButtons();
+
+  arrowLeft.addEventListener('click', function() {
+    currentFrame--;
+  
+    carouselInner.style.transform = `translateX(-${currentFrame*offcetWidth}px)`;
+
+    showButtons();
+  });
+
+  arrowRight.addEventListener('click',function() {
+    currentFrame++;
+
+    carouselInner.style.transform = `translateX(-${currentFrame*offcetWidth}px)`;
+
+    showButtons();
+  });
+
+
 }
